6459b1653966109beca0960cd8537384dcd1b26a
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2016-11-14 20:08:45 +0100

    linear ramp in test signal

--- generate.py
+++ generate.py
@@ -32,6 +32,12 @@ def dbramp(fs, t, l1, l2):
     slope = float(l2 - l1) / T
     return lvl(l1 + slope * np.arange(T))
 
+def linramp(fs, t, l1, l2):
+    T = int(t * fs)
+    a1, a2 = lvl(l1), lvl(l2)
+    slope = float(a2 - a1) / T
+    return a1 + slope * np.arange(T)
+
 def lvl(db):
     return 10**(db/20.)
 
@@ -43,6 +49,8 @@ audio = [
     silence(fs, 1),
     lvl(-30) * logsweep(fs, 20, 10, 40000),
     silence(fs, 1),
+    linramp(fs, 10, -30, 0) * sin(fs,   315, 10),
+    linramp(fs, 10, -30, 0) * sin(fs,  1000, 10),
     dbramp(fs, 5, -30, 0) * sin(fs,   315, 5),
     dbramp(fs, 5, -30, 0) * sin(fs,  1000, 5),
     dbramp(fs, 5, -30, 0) * sin(fs,  6300, 5),
@@ -53,7 +61,7 @@ audio = [
     silence(fs, 5),
 ]
 
-wavfile.write('out.wav', fs,
+wavfile.write('testsignal.wav', fs,
               np.array(np.concatenate(audio) * np.iinfo(np.int32).max,
               dtype=np.int32))
 
