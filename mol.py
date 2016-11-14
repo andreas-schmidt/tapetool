5bb7b084b605aa75bc74a09ba280a0c7bf5944a6
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2016-11-15 00:15:50 +0100

    concept for MOL measurement

---
+++ mol.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+from tapetool.helpers import read_wav, db, rms, timeslice
+from tapetool.filters import thd_for_1k
+
+fs, data = read_wav("05-1905.wav")
+
+reflevel = data[16*fs:26*fs]
+thd_rl = 100. * rms(thd_for_1k(fs, reflevel)) / rms(reflevel) 
+print("THD ad reference level:", thd_rl)
+
+ramp = data[58*fs:68*fs]
+sig = list(timeslice(fs, ramp))
+thd = list(timeslice(fs, thd_for_1k(fs, ramp)))
+
+rl = db(rms(reflevel))
+out = open('mol', 'w')
+for a, b in zip(sig, thd):
+    i, t, l1 = a
+    _, _, l2 = b
+    x = db(l1) - rl
+    k = 100. * l2 / l1
+    print(*(t, ) + tuple(x) + tuple(k), file=out)
