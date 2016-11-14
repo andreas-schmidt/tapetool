9cd7f22961c84687e26404d58209d7077ea1c5c7
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2016-11-14 22:43:59 +0100

    made klirr a straightforward script

--- klirr.py
+++ klirr.py
@@ -1,21 +1,15 @@
-# coding: utf-8
-
 from helpers import read_wav, db, rms
 import filters
 
-def main(filename):
+for filename in (
+        '1k+3k75+25Summe.wav',
+        '1k+3klr97+3summe.wav',
+        '1k+3klr3ProzSumme.wav'):
     fs, data = read_wav(filename)
-    db_sig, db_h3, k3 = calc_k3(fs, data)
-    print '{:22}: total {:2.1f}dB, h3 {:2.1f}dB, k3 {:5.2f}%'.format(
-        filename, db_sig, db_h3, k3
-    )
-
-def calc_k3(fs, data):
     h3 = filters.thd_for_1k(fs, data)
-    return db(rms(data)), db(rms(h3)), 100. * rms(h3) / rms(data)
-
-if __name__ == '__main__':
-    main('1k+3k75+25Summe.wav')
-    main('1k+3klr97+3summe.wav')
-    main('1k+3klr3ProzSumme.wav')
-
+    print '{:22}: total {:2.1f}dB, h3 {:2.1f}dB, k3 {:5.2f}%'.format(
+        filename,
+        db(rms(data)),
+        db(rms(h3)),
+        100. * rms(h3) / rms(data)
+    )
