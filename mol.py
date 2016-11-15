9a704f6d488ee10eb338ab7240317d01350dba91
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2016-11-15 15:42:27 +0100

    moved MOL logic to analysis.py

--- mol.py
+++ mol.py
@@ -1,20 +1,14 @@
 from __future__ import print_function
 
 from tapetool.helpers import read_wav, db, rms
-from tapetool.analysis import timeslice
+from tapetool.analysis import timeslice, mol_1k
 from tapetool.filters import thd_for_1k
 
 fs, data = read_wav("05-1905.wav")
+data_volramp = data[58*fs:68*fs]
+dt = 0.05
 
-reflevel = data[16*fs:26*fs]
-thd_rl = 100. * rms(thd_for_1k(fs, reflevel)) / rms(reflevel) 
-print("THD at reference level:", thd_rl)
-
-ramp = data[58*fs:68*fs]
-sig_x, sig_y = timeslice(fs, ramp, 0.05)
-_, thd_y = timeslice(fs, thd_for_1k(fs, ramp), 0.05)
-
-rl = db(rms(reflevel))
 with open('mol.dat', 'w') as out:
-    for t, a, b in zip(sig_x, sig_y, thd_y):
-        print(t, db(a) - rl[0], 100. * b / a, file=out)
+    for i in zip(*mol_1k(fs, data_volramp, dt)):
+        print(*i, file=out)
+
--- tapetool/analysis.py
+++ tapetool/analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
-from helpers import rms
+from helpers import rms, db
+import filters
 
 def it_timeslice(fs, data, dt):
     bs = int(dt * fs)
@@ -12,3 +13,16 @@ def it_timeslice(fs, data, dt):
 def timeslice(fs, data, dt, channel=0):
     x, y = zip(*list(it_timeslice(fs, data, dt)))
     return np.array(x), np.array(y)[:,channel]
+
+def mol(fs, data, dt, bpf):
+    t, signal = timeslice(fs, data, dt)
+    _, harm3 = timeslice(fs, bpf(fs, data), dt)
+    thd = 100. * harm3 / signal
+    return t, db(signal), thd
+
+def mol_1k(fs, data, dt):
+    return mol(fs, data, dt, filters.thd_for_1k)
+
+def mol_315(fs, data, dt):
+    return mol(fs, data, dt, filters.thd_for_315)
+
