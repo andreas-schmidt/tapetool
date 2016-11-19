6f6ea5869cd2f14008a940b1a27907cdceb90a3d
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2016-11-20 00:12:09 +0100

    shorter signals

--- testsignal.py
+++ testsignal.py
@@ -6,39 +6,39 @@ from tapetool.helpers import lvl, write_wav
 fs = 96000
 
 audio = [
-    silence(fs, 5),
-    lvl(-10) * sin(fs, 10, 315),
     silence(fs, 1),
-    linramp(fs, 10, -30, 0) * sin(fs, 10,   315),
-    silence(fs, 1),
+    lvl(-10) * sin(fs, 5, 315),
+    silence(fs, 0.5),
+    linramp(fs, 5, -30, 0) * sin(fs, 5,   315),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5,  6300),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 10000),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 12500),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 14000),
-    silence(fs, 5),
+    silence(fs, 1),
 ]
 
 write_wav(fs, np.concatenate(audio), 'testsignal-slow.wav')
 
 audio = [
-    silence(fs, 5),
-    lvl(-10) * sin(fs, 10, 1000),
     silence(fs, 1),
-    linramp(fs, 10, -30, 0) * sin(fs, 10,  1000),
-    silence(fs, 1),
+    lvl(-10) * sin(fs, 5, 1000),
+    silence(fs, 0.5),
+    linramp(fs, 5, -30, 0) * sin(fs, 5,  1000),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5,  6300),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 10000),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 12500),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 14000),
-    silence(fs, 1),
+    silence(fs, 0.5),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 16000),
-    silence(fs, 5),
+    silence(fs, 1),
 ]
 
 write_wav(fs, np.concatenate(audio), 'testsignal-fast.wav')
