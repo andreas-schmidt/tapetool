e0d5dc5e009a9d70ce5258a3f210f6efbc08cda6
Andreas Schmidt <andreas.schmidt.42@gmx.net>
2016-11-20 00:02:15 +0100

    shorter testsignals per speed

--- testsignal.py
+++ testsignal.py
@@ -9,21 +9,37 @@ audio = [
     silence(fs, 5),
     lvl(-10) * sin(fs, 10, 315),
     silence(fs, 1),
+    linramp(fs, 10, -30, 0) * sin(fs, 10,   315),
+    silence(fs, 1),
+    dbramp(fs, 5, -30, 0) * sin(fs, 5,  6300),
+    silence(fs, 1),
+    dbramp(fs, 5, -30, 0) * sin(fs, 5, 10000),
+    silence(fs, 1),
+    dbramp(fs, 5, -30, 0) * sin(fs, 5, 12500),
+    silence(fs, 1),
+    dbramp(fs, 5, -30, 0) * sin(fs, 5, 14000),
+    silence(fs, 5),
+]
+
+write_wav(fs, np.concatenate(audio), 'testsignal-slow.wav')
+
+audio = [
+    silence(fs, 5),
     lvl(-10) * sin(fs, 10, 1000),
     silence(fs, 1),
-    lvl(-30) * logsweep(fs, 20, 10, 40000),
-    silence(fs, 1),
-    linramp(fs, 10, -30, 0) * sin(fs, 10,   315),
     linramp(fs, 10, -30, 0) * sin(fs, 10,  1000),
-    dbramp(fs, 5, -30, 0) * sin(fs, 5,   315),
-    dbramp(fs, 5, -30, 0) * sin(fs, 5,  1000),
+    silence(fs, 1),
     dbramp(fs, 5, -30, 0) * sin(fs, 5,  6300),
+    silence(fs, 1),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 10000),
+    silence(fs, 1),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 12500),
+    silence(fs, 1),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 14000),
+    silence(fs, 1),
     dbramp(fs, 5, -30, 0) * sin(fs, 5, 16000),
     silence(fs, 5),
 ]
 
-write_wav(fs, np.concatenate(audio), 'testsignal.wav')
+write_wav(fs, np.concatenate(audio), 'testsignal-fast.wav')
 
